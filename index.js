920adfaf5dcf7e105496e4af4ab0cdf6a4a8961f
xITSDUCKYx <82958945+xITSDUCKYx@users.noreply.github.com>
2023-03-06 14:32:51 -0500

    Digital-Business-Card
    Basic stuff

---
+++ index.js
@@ -0,0 +1,12 @@
+// make it change photos and info based off data 
+
+// possibly make data info cycle through my personal projects 
+
+// add radial lighting that follows the pointer
+
+// make font parameters constantly changing
+
+// try the neon light effects for the info https://www.youtube.com/watch?v=JbjA4oE4Ma4&t=269s
+
+// add curves https://www.youtube.com/watch?v=lPJVi797Uy0&t=6s
+
